59086b9c318d22bf503c7d86cf70575cb37c8725
Scott Lieber <WSox1235@aol.com>
2018-02-22 01:59:27 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,29 @@
+$("img", "#gameoflife").hover(function() {
+  $('.gamestill').toggle();
+  $('.gamegif').toggle();
+});
+
+$("img", "#simon").hover(function() {
+  $('.simonstill').toggle();
+  $('.simongif').toggle();
+});
+
+$("img", "#pomodoro").hover(function() {
+  $('.pomostill').toggle();
+  $('.pomogif').toggle();
+});
+
+$("img", "#leaderboard").hover(function() {
+  $('.leaderstill').toggle();
+  $('.leadergif').toggle();
+});
+
+$("img", "#nytimes").hover(function() {
+  $('.nytstill').toggle();
+  $('.nytgif').toggle();
+});
+
+$("img", "#calc").hover(function() {
+  $('.calcstill').toggle();
+  $('.calcgif').toggle();
+});
